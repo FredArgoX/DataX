631f1b1c62a570dd1b47173500c7974ca4db29e0
FredArgoX <fredy.argo.3@gmail.com>
2025-04-15 23:18:15 -0600

    Update LinRegresn.py

--- python_guides/LinRegresn.py
+++ python_guides/LinRegresn.py
@@ -5,7 +5,7 @@ import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split # Splitting data
 from sklearn.linear_model import LinearRegression # Linear Regression
-from sklearn.metrics import mean_squared_error, r2_score # Model evaluation
+from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error # Model evaluation
 import matplotlib.pyplot as plt # Visualization
 
 # --------------------------------------------------------------------------
